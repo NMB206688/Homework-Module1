867a1b3e0fdf2c2738f4cb9f394da73b5d802cd8
Naveen Mamidi <n.mamidi.247@westcliff.edu>
2024-06-04 09:20:31 -0700

    Added Text Verification in Conact html filea and script to it

--- JS/randomcodes.js
+++ JS/randomcodes.js
@@ -4,12 +4,12 @@ function generateCode(){
 
     for(i=1; i<= 8; i++)
         {
-            var char=Math.random()*str.length;
+            var char=Math.random() * str.length;
             code += str.charAt(char)
         }
         return code;
 }
-document.getElementById(codes).innerHTML = generateCode();
+document.getElementById("codes").innerHTML = generateCode();
 
 function disableButton(){
     document.getElementById("submit").disabled = true;
